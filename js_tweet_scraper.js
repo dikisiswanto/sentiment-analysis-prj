26920dab7f484854d35e691852ce65f7a7f91a95
Diki Siswanto <dickyachmady@gmail.com>
2023-07-09 09:52:33 +0800

    first commit

---
+++ js_tweet_scraper.js
@@ -0,0 +1,40 @@
+let i = 1;
+let dict = {};
+let like_regex = /\b(\d+) Like|Likes\b/;
+let retweet_regex = /\b(\d+) Retweet|Retweets\b/;
+let reply_regex = /\b(\d+) Repl(ies|y)\. Reply\b/;
+let time_regex = /\btime datetime=\"(.*?)\">\b/;
+let status_regex = /\b\/status\/(\d+)\b/;
+
+function loop() {
+	setTimeout(function () {
+		window.scrollBy(0, 1000);
+		let element = document.querySelector('[data-testid=tweet]');
+		for (let index = 0; index < element.length; index++) {
+			try {
+				let reply = element[index].innerHTML.match(reply_regex);
+				let like = element[index].innerHTML.match(like_regex);
+				let retweet = element[index].innerHTML.match(retweet_regex);
+				let time = element[index].innerHTML.match(time_regex);
+				let status = element[index].innerHTML.match(status_regex);
+				dict[element[index].innerText.split("\n")[1].concat("-" + time[1])] = {
+					"like": `${like[1]}`,
+					"reply": `${reply[1]}`,
+					"retweet": `${retweet[1]}`,
+					"time": `${time[1]}`,
+					"username": `${element[index].innerText.split("\n")[1]}`,
+					"text": `${element[index].querySelector('[data-testid=tweetText]').innerText}`,
+					"status": `${status[1]}`
+				};
+			} catch (error) {
+				console.log(error);
+				throw new Error(error);
+			}
+		}
+		i++;
+		if (i < 100000) {
+			loop();
+		}
+	}, 500)
+}
+loop();
