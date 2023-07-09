795687ddb9174d4ee35f47979fdbc40f6c2359cf
Diki Siswanto <dickyachmady@gmail.com>
2023-07-09 09:57:44 +0800

    chore: fix js_tweet_scraper and add tweet query

--- js_tweet_scraper.js
+++ js_tweet_scraper.js
@@ -1,29 +1,29 @@
 let i = 1;
-let dict = {};
-let like_regex = /\b(\d+) Like|Likes\b/;
-let retweet_regex = /\b(\d+) Retweet|Retweets\b/;
-let reply_regex = /\b(\d+) Repl(ies|y)\. Reply\b/;
-let time_regex = /\btime datetime=\"(.*?)\">\b/;
-let status_regex = /\b\/status\/(\d+)\b/;
+const dict = {};
+const like_regex = /\b(\d+) Like|Likes\b/;
+const retweet_regex = /\b(\d+) Retweet|Retweets\b/;
+const reply_regex = /\b(\d+) Repl(ies|y)\. Reply\b/;
+const time_regex = /\btime datetime=\"(.*?)\">\b/;
+const status_regex = /\b\/status\/(\d+)\b/;
 
 function loop() {
 	setTimeout(function () {
 		window.scrollBy(0, 1000);
-		let element = document.querySelector('[data-testid=tweet]');
-		for (let index = 0; index < element.length; index++) {
+		let elements = document.querySelectorAll('[data-testid=tweet]');
+		for (let index = 0; index < elements.length; index++) {
 			try {
-				let reply = element[index].innerHTML.match(reply_regex);
-				let like = element[index].innerHTML.match(like_regex);
-				let retweet = element[index].innerHTML.match(retweet_regex);
-				let time = element[index].innerHTML.match(time_regex);
-				let status = element[index].innerHTML.match(status_regex);
-				dict[element[index].innerText.split("\n")[1].concat("-" + time[1])] = {
+				let reply = elements[index].innerHTML.match(reply_regex);
+				let like = elements[index].innerHTML.match(like_regex);
+				let retweet = elements[index].innerHTML.match(retweet_regex);
+				let time = elements[index].innerHTML.match(time_regex);
+				let status = elements[index].innerHTML.match(status_regex);
+				dict[elements[index].innerText.split("\n")[1].concat("-" + time[1])] = {
 					"like": `${like[1]}`,
 					"reply": `${reply[1]}`,
 					"retweet": `${retweet[1]}`,
 					"time": `${time[1]}`,
-					"username": `${element[index].innerText.split("\n")[1]}`,
-					"text": `${element[index].querySelector('[data-testid=tweetText]').innerText}`,
+					"username": `${elements[index].innerText.split("\n")[1]}`,
+					"text": `${elements[index].querySelector('[data-testid=tweetText]').innerText}`,
 					"status": `${status[1]}`
 				};
 			} catch (error) {
